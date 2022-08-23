250e0ef6b3b940147d2207c9830dcfc9816227d6
Robocraft999 <Alexakeil987@gmail.com>
2022-08-23 12:48:39 +0200

    added Level logic and moved Slash handler

--- src/main/java/com/github/robocraft999/lib/GuildCommand.java
+++ src/main/java/com/github/robocraft999/lib/GuildCommand.java
@@ -1,14 +1,29 @@
 package com.github.robocraft999.lib;
 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import com.github.robocraft999.modules.tiers.TierManager.Level;
+
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 public abstract class GuildCommand extends BasicCommand{
-
-	public GuildCommand(String command, String description) {
+	
+	private Level level = Level.LOW;
+	private boolean handlesAccessItself = false;
+	
+	public GuildCommand(String command, String description, Level level) {
 		super(command, description);
+		this.level = level;
+	}
+	
+	public GuildCommand(String command, String description, boolean handlesAccessItself) {
+		super(command, description);
+		this.handlesAccessItself = handlesAccessItself;
 	}
 
 	/**
@@ -22,5 +37,31 @@ public abstract class GuildCommand extends BasicCommand{
 	@Override
 	public Message perform(String[] args, User author, TextChannel channel) {
 		return perform(args, channel.getGuild().getMember(author), channel);
+	}
+	
+	public void performSlash(SlashCommandInteractionEvent event) {
+		ArrayList<String> argsList = new ArrayList<>(event.getOptions().stream().map(om -> om.getAsString()).toList());
+		argsList.add(0, event.getName());
+		String[] args = argsList.toArray(new String[0]);
+		Message msg = perform(args, event.getUser(), event.getChannel().asTextChannel());
+		if(msg != null) {
+			String[] m = processDelayArg(msg.getContentRaw());//TODO maybe change to display
+			if(m.length == 2) {
+				int delay = Integer.parseInt(m[1]);
+				event.reply(m[0]).complete().deleteOriginal().queueAfter(delay, TimeUnit.SECONDS);
+			}else {
+				event.reply(m[0]).queue();
+			}
+		}else {
+			event.reply("No Answer").complete().deleteOriginal().queueAfter(5, TimeUnit.SECONDS);
+		}
+	}
+
+	public Level getLevel() {
+		return level;
+	}
+	
+	public boolean handlesAccessItself() {
+		return handlesAccessItself;
 	}
 }
